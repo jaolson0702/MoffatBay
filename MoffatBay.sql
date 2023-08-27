11c7f6df0027173405d0df35dc819efc5187ab8c
jaolson0702 <jaolson.0702@gmail.com>
2023-08-27 12:44:18 -0500

    Removed separate SQL files and created single SQL file for creating and populating database.

--- MoffatBay.sql
+++ MoffatBay.sql
@@ -30,3 +30,21 @@ CREATE TABLE Bookings (
     CONSTRAINT FK_Bookings_Rooms FOREIGN KEY (rooms_id) REFERENCES Rooms(id),
     CONSTRAINT FK_Bookings_Customers FOREIGN KEY (customers_id) REFERENCES Customers(id)
 );
+
+INSERT INTO rooms (id, room_size, available)
+VALUES  (101, "double full beds", TRUE),
+        (102, "queen bed", TRUE),
+        (103, "double queen beds", TRUE),
+        (104, "king bed", TRUE),
+        (105, "double full beds", TRUE);
+
+insert into Customers (id,first_name, last_name, email, phone, password)
+VALUES  (1, "Anh", "Vo", "bellevueuni1@gmail.com", "402-717-8900","$ummeR2023"),
+        (2, "Jared", "Olsen", "bellevueuni2@gmail.com", "402-717-8999","$ummeR2024"),
+        (3, "Charlene", "Centeno", "bellevueuni3@gmail.com", "402-717-4545","$ummeR2025"),
+        (4, "Bryce", "Kellas", "bellevueuni4@gmail.com", "402-717-8787","$ummeR2026");
+        
+INSERT INTO bookings (guest_count, check_in, check_out, rooms_id, customers_id)
+VALUES  (5, '2024-1-1', '2024-2-1', 103, 4),
+        (2, '2024-10-29', '2024-10-31', 104, 2),
+        (3, '2024-9-13', '2024-9-13', 101, 1);
--- MoffatBayInsertRooms.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO rooms (id, room_size, available)
-VALUES  (101, "double full beds", TRUE),
-        (102, "queen bed", TRUE),
-        (103, "double queen beds", TRUE),
-        (104, "king bed", TRUE),
-        (105, "double full beds", TRUE),
-        (106, "queen bed", TRUE),
-        (107, "double queen beds", TRUE),
-        (108, "king bed", TRUE);
