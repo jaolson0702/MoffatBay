de0dd8dd470a8223df8ff414f5a2ffe416c23e58
brykellas <111330215+brykellas@users.noreply.github.com>
2023-09-02 12:21:13 -0700

    moved models and beans under lodge package

---
+++ moffatbaylodge/src/main/java/lodge/beans/Customer.java
@@ -0,0 +1,63 @@
+/* Team Delta
+ * Authors: Bryce Kellas
+ */
+package lodge.beans;
+
+import java.io.Serializable;
+
+public class Customer implements Serializable {
+    int id;
+    String firstName;
+    String lastName;
+    String email;
+    String phoneNumber;
+    String password;
+
+    // Contructor
+    public Customer() {
+        this.id = 0;
+        this.firstName = "";
+        this.lastName = "";
+        this.email = "";
+        this.phoneNumber = "";
+        this.password = "";
+    }
+    
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
