96f2c859419b510e0f00289cb7e989d55098de26
brykellas <111330215+brykellas@users.noreply.github.com>
2023-09-01 20:09:17 -0700

    Created Customer bean

---
+++ moffatbaylodge/src/main/java/beans/Customer.java
@@ -0,0 +1,58 @@
+package beans;
+
+public class Customer {
+    int id;
+    String firstName;
+    String lastName;
+    String email;
+    String phoneNumber;
+    String password;
+
+    // Contructor
+    public Customer(int id, String firstName, String lastName, String email, String phoneNumber, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+    
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
