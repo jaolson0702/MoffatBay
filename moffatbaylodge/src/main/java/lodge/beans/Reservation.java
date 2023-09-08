7bb359d6699d2cc94652527f6a3f181030623dbb
jaolson0702 <jaolson.0702@gmail.com>
2023-09-08 14:05:48 -0500

    Created basic skeleton for ReservationServlet class and Reservation bean; added configurations for ReservationServlet in web.xml.

---
+++ moffatbaylodge/src/main/java/lodge/beans/Reservation.java
@@ -0,0 +1,36 @@
+package lodge.beans;
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.time.LocalDate;
+
+public class Reservation implements Serializable {
+    int id;
+    int roomsId;
+    int customersId;
+    int guestCount;
+    Date checkIn;
+    Date checkOut;
+
+    public Reservation() {
+        this.id = 0;
+        this.roomsId = 0;
+        this.customersId = 0;
+        this.guestCount = 0;
+        this.checkIn = Date.valueOf(LocalDate.now());
+        this.checkOut = Date.valueOf(LocalDate.now());
+    }
+
+    public int getId() {return id;}
+    public void setId(int id) {this.id = id;}
+    public int getRoomsId() {return roomsId;}
+    public void setRoomsId(int roomsId) {this.roomsId = roomsId;}
+    public int getCustomersId() {return customersId;}
+    public void setCustomersId(int customersId) {this.customersId = customersId;}
+    public int getGuestCount() {return guestCount;}
+    public void setGuestCount(int guestCount) {this.guestCount = guestCount;}
+    public Date getCheckIn() {return checkIn;}
+    public void setCheckIn(Date checkIn) {this.checkIn = checkIn;}
+    public Date getCheckOut() {return checkOut;}
+    public void setCheckOut(Date checkOut) {this.checkOut = checkOut;}
+}
