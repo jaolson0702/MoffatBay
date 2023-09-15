31da6c4583c6fd6ebdd29537b2d38494a3d23558
brykellas <111330215+brykellas@users.noreply.github.com>
2023-09-14 21:40:29 -0700

    Registration form validations implemented

--- moffatbaylodge/src/main/java/lodge/filters/ValidationFilter.java
+++ moffatbaylodge/src/main/java/lodge/filters/ValidationFilter.java
@@ -1,13 +1,18 @@
 package lodge.filters;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.commons.lang3.Validate;
 
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.FilterConfig;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import lodge.beans.Validator;
 
 
 
@@ -19,10 +24,39 @@ public class ValidationFilter implements Filter  {
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        System.out.println("ValidationFilter");
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println(request.getParameter("form-name"));
+        Validator v = new Validator();
+
+        String formName = request.getParameter("form-name");
+        if (formName != null) {
+            switch (formName) {
+                case "register": 
+                    boolean b1 = v.isFirstNameValid(request.getParameter("firstname"));
+                    boolean b2 = v.isLastNameValid(request.getParameter("lastname"));
+                    boolean b3 = v.isPhoneValid(request.getParameter("phone"));
+                    boolean b4 = v.IsEmailValid(request.getParameter("email"));
+                    boolean b5 = v.isPasswordValid(request.getParameter("psw"));
+                    if (!b1 || !b2 || !b3 || !b4 || !b5) {
+                        ArrayList<String> errors = v.getErrorMessages();
+                        request.setAttribute("errors", errors);
+                        RequestDispatcher rd = request.getRequestDispatcher("?action=registration");
+                        rd.include(request, response);
+                    }
+                    break;
+                case "login":
+                    
+                    break;
+                case "reserve":
+                    
+                    break;
+                case "lookup":
+                    
+                    break;              
+                default:
+                    break;
+            }
+        }
         chain.doFilter(request, response);
     }
-    
 }
